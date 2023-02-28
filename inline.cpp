3173398dcf0548683bc91f29d75b9f9f8424c752
mukul0708 <112548350+mukul0708@users.noreply.github.com>
2023-02-28 12:45:21 +0530

    add

---
+++ inline.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class sum{
+    public:
+    inline int sum(int x,int y){
+        return x+y;
+    }
+}
+
+int main(){
+    class sum s1;
+    cout <<"Sum is " <<s1.summ(10,40);
+return 0;
+}
