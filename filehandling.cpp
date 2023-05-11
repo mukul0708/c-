72ec1c1b53ff740ce80741e6a0f00434519d0cda
mukul0708 <112548350+mukul0708@users.noreply.github.com>
2023-05-11 12:07:20 +0530

    1

--- filehandling.cpp
+++ filehandling.cpp
@@ -3,10 +3,14 @@ using namespace std;
 
 int main()
 {
-    #ifdef ONLINE_JUDGE
-    freopen("input.txt","r");
-    freopen("output.txt","w");
-    #endif
+    // #ifdef ONLINE_JUDGE
+    // freopen("input.txt","r",ios::in);
+    // freopen("output.txt","w",ios::out);
+    // #endif
 
+    fstream f1;
+    f1.open("file.txt","w",ios::out);
+    obj<<"Hello \n";
+    obj.close();
 	return 0;
 }
